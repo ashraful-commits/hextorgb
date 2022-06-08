0f103fd8bb4eae70545045d83945341226a62c50
Md.Ashraful Alam <ashrafulalam10000@gmail.com>
2022-06-08 16:24:53 +0600

    new commit

--- app.js
+++ app.js
@@ -2,18 +2,35 @@ const colorForm =document.getElementById('colorForm');
 const invalidAlert = document.querySelector('.msg');
 const resultAlert = document.querySelector('.result');
 
+
+
     // input hex color 
 let hexCode = colorForm.querySelector('input[name="hex"]');
+
+hexCode.oninput=()=>{
+    hexText.style.borderBottom ='1px solid white';
+    hexText.style.borderRadius = "5px"; 
+}
+
     // label text 
+
+
+
 let hexText = document.querySelector('.hexLabelText');
 let rgbText = document.querySelector('.rgbLabelText');
 
-hexCode.oninput=()=>{
-     hexText.style.borderBottom ='1px solid gray'
-     hexText.style.backgroundColor =hexCode.value;
-     hexText.style.borderRadius = "5px";
-    
+
+
+
+// color input 
+
+
+let color =colorForm.querySelector('input[name="color"]')
+color.oninput=()=>{
+    hexCode.value =color.value
 }
+
+
 
 
 // form submit 
@@ -22,7 +39,14 @@ colorForm.addEventListener('submit',(e)=>{
     e.preventDefault();
 
     // input rgb color 
+
+
+
     let rgbCode = colorForm.querySelector('input[name="rgb"]');
+    let r_code =document.getElementById('r')
+    let g_code =document.getElementById('g')
+    let b_code =document.getElementById('b')
+ 
      
     // form validation or result 
 
@@ -30,16 +54,27 @@ colorForm.addEventListener('submit',(e)=>{
         invalidAlert.innerHTML = setAlert('Hex color field is requird')
    }else if(testPattern(hexCode.value) == false){
         invalidAlert.innerHTML = setAlert('Invalid HEX COLOR code [exm:#0af0fe]','danger')
+        
+
+// final value 
+
     }else{
         invalidAlert.innerHTML = '';
         let hex_value = hexCode.value;
         let r =parseInt(hex_value.slice(1,3),16);
         let g =parseInt(hex_value.slice(3,5),16);
         let b =parseInt(hex_value.slice(5,7),16);
-        resultAlert.value = `rgb(${r},${g},${b})`;
+        resultAlert.value = `RGB(${r},${g},${b})`;
         rgbText.style.borderBottom = '1px solid gray';
-        rgbText.style.backgroundColor = hexCode.value;
-        rgbText.style.borderRadius = "5px";
+
+
+
+// r/g/b code show here 
+
+
+        r_code.value = r;
+        g_code.value = g;
+        b_code.value = b;
     }
 
 })
